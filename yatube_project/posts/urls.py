b3b8e1109eaff10c88aa67b171e408bc56472f2a
Rolan Imangulov <imangulov.roman2015@gmail.com>
2023-06-18 19:58:09 +0500

    Changed urls.py and functions in views.py.

--- yatube_project/posts/urls.py
+++ yatube_project/posts/urls.py
@@ -1,7 +1,8 @@
 from . import views
 from django.urls import path
 
+app_name = 'posts'
 urlpatterns = [
-    path('', views.index),
-    path('group/<slug:slug>/', views.group_posts),
+    path('', views.index, name='index'),
+    path('group/<slug:slug>/', views.group_posts, name='group_posts'),
 ]
--- yatube_project/posts/views.py
+++ yatube_project/posts/views.py
@@ -5,16 +5,14 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('<h1>The main page of the Yatube project.<h1>'
-                        '<p>About</p>'
-                        '<h2>There will be posts about'
-                            '<ul>'
-                                '<li><a href="https://i.pinimg.com/originals/aa/d2/99/aad2991ed6b54233f382d5a12a01d61d.jpg" target="_blank">dogs</a></li>'
-                                '<li><a href="https://ferret-pet.ru/wp-content/uploads/4/1/c/41c114082ccae8adaee31f4f6e797d2b.jpeg" target="_blank">cats</a></li>'
-                                '<li><i>people</i></li>'
-                            '</ul>'
-                        '</h2>')
+    template = 'posts/index.html'
+    title = '<h4>Это главная страница проекта Yatube</h4>'
+    context = {'title': title}
+    return render(request, template, context)
 
 
 def group_posts(request, slug):
-    return HttpResponse(f'Posts group by {slug}.')
+    template = 'posts/group_list.html'
+    title = '<h4>Здесь будет информация о группах проекта Yatube</h4>'
+    context = {'title': title}
+    return render(request, template, context)
