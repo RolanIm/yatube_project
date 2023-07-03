06e48c46f945100423a11a0e2013a30945f48294
Rolan_Imangulov <imangulov.roman2015@gmail.com>
2023-07-03 11:45:01 +0500

    Created the new app - "about".
     Added html template author and Class Based View.

--- yatube_project/yatube_project/settings.py
+++ yatube_project/yatube_project/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'about.apps.AboutConfig',
     'core.apps.CoreConfig',
     'users.apps.UsersConfig',
     'posts.apps.PostsConfig',
